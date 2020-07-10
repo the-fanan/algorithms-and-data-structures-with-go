80151f33003732dd6787cfec4bb1c52bbbc402c3
Fanan Dala <fanan.dala@yahoo.com>
2020-07-10 15:38:09 +0100

    updated remove elements

--- exercises/leetcode/move-zeros/main.go
+++ exercises/leetcode/move-zeros/main.go
@@ -9,21 +9,15 @@ func main(){
 }
 
 func moveZeroes(nums []int) []int{
-	p1 := 0
-	p2 := 0
-
-	for p2 < len(nums) && p1 < len(nums) {
-		if nums[p1] == 0 {
-			if nums[p2] != 0 {
-				nums[p1] = nums[p2]
-				nums[p2] = 0
-				p1++
-			}
-			p2++
-		} else {
-			p1++
-			p2++
+	i := 0
+	for j := 0; j < len(nums); j++ {
+		if nums[j] != 0 {
+			nums[i] = nums[j];
+			i++
 		}
+	}
+	for k := i; k < len(nums); k++{
+		nums[k] = 0
 	}
 	return nums
 }
