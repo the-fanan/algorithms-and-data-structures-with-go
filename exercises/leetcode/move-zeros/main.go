d1de695eb06f263b319791aeaf6c6151e40ebb8c
Fanan Dala <fanan.dala@yahoo.com>
2020-07-10 15:41:14 +0100

    updated move zeros

--- exercises/leetcode/move-zeros/main.go
+++ exercises/leetcode/move-zeros/main.go
@@ -21,3 +21,23 @@ func moveZeroes(nums []int) []int{
 	}
 	return nums
 }
+
+func moveZeroes2(nums []int) []int{
+	p1 := 0
+	p2 := 0
+
+	for p2 < len(nums) && p1 < len(nums) {
+		if nums[p1] == 0 {
+			if nums[p2] != 0 {
+				nums[p1] = nums[p2]
+				nums[p2] = 0
+				p1++
+			}
+			p2++
+		} else {
+			p1++
+			p2++
+		}
+	}
+	return nums
+}
