3aa0fdcfcee9a9eed62e0ce6e44ea975399a5823
Fanan Dala <fanan.dala@yahoo.com>
2020-07-21 00:09:55 +0100

    completed solution

--- exercises/leetcode/medium/delete-nodes-and-return-forest/main.go
+++ exercises/leetcode/medium/delete-nodes-and-return-forest/main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	r := &TreeNode{Val:1}
-
-	fmt.Println(delNodes(r,[]int{3,5}))
+	r.Left = &TreeNode{Val:2}
+	r.Right = &TreeNode{Val:3}
+	r.Right.Right = &TreeNode{Val:4}
+	forests := delNodes(r,[]int{2,1})
+	for _, root := range forests {
+		fmt.Println(root.Val)
+	}
 }
 
 /*
@@ -23,9 +28,81 @@ type TreeNode struct {
 
 func delNodes(root *TreeNode, to_delete []int) []*TreeNode {
 	fr := make([]*TreeNode,0)
+	type ToDelete struct {
+		Parent *TreeNode 
+		ChildPos string
+	}
+	toDeletes := make([]*ToDelete,0)
 	for _,vtd := range to_delete {
-			fmt.Println(vtd)
+		node, childPosition := find(root, vtd)
+		td := &ToDelete{Parent: node, ChildPos: childPosition}
+		toDeletes = append(toDeletes,td)
+		child := &TreeNode{}
+		if childPosition == "left" {
+			child = node.Left
+		} else if childPosition == "right" {
+			child = node.Right
+		} else {
+			//if neither left nor right return a value then the value must be root
+			child = root
+		}
+		if child.Left != nil {
+			fr = append(fr,child.Left)
+		}
+		if child.Right != nil {
+			fr = append(fr,child.Right)
+		}
+	}
+	//do the actual deletion
+	for _, td := range toDeletes {
+		if td.ChildPos == "left" {
+			td.Parent.Left = nil
+		} else if td.ChildPos == "right"  {
+			td.Parent.Right = nil
+		}
 	}
 	
+	if root != nil {
+		fr = append(fr,root)
+	}
+	//remove nodes contained in to delete list
+	for _, tdv := range to_delete {
+		for i,n := range fr {
+			if tdv == n.Val {
+				fr[len(fr)-1], fr[i] = fr[i], fr[len(fr)-1]
+				fr = fr[:len(fr)-1]
+				break
+			}
+		}
+	}
 	return fr
+}
+
+func find(r *TreeNode, v int) (*TreeNode, string) {
+	if r == nil {
+		return nil, ""
+	}
+
+	if r.Left != nil {
+		if r.Left.Val == v {
+			return r, "left"
+		} else {
+			n,p := find(r.Left, v)
+			if n != nil {
+				return n,p
+			}
+		}
+	}
+
+	if r.Right != nil {
+		if r.Right.Val == v {
+			return r, "right"
+		} else {
+			n,p := find(r.Right, v)
+			if n != nil {
+				return n,p
+			}
+		}
+	}
+	return nil, ""
 }
